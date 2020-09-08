29fe9c29779c4041e713610d7b6e4b9f80469eac
jultib3t <61161014+jultib3t@users.noreply.github.com>
2020-09-08 13:03:42 +0300

    Update force.php

--- force.php
+++ force.php
@@ -3,11 +3,16 @@
 * Force You welcome Message to be display all the time
 */
 
-add_action( 'load-index.php', 'show_welcome_panel' );
+function wrapper(){
+ add_action( 'load-index.php', 'show_welcome_panel' );
 
 function show_welcome_panel() {
+    // get the current user ID
     $user_id = get_current_user_id();
-
-    if ( 1 != get_user_meta( $user_id, 'show_welcome_panel', true ) )
-        update_user_meta( $user_id, 'show_welcome_panel', 1 );
+    
+    if ( 1 != get_user_meta( $user_id, 'show_welcome_panel', true ) ) // check if the welcome message is true or not in the datavase
+        update_user_meta( $user_id, 'show_welcome_panel', 1 ); // Update the user meta
+ }   
 }
+
+add_action( 'admin_init', 'wrapper');
